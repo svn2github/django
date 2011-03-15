svn2github/django

commit 28f83e1fd04b94e6f1806b2167926123d09a56a7

Fixed #15563 -- Update the LANG_INFO dictionary with newly added translations az, es-mx and ur. Thanks, lasko.
git-svn-id: https://code.djangoproject.com/svn/django/trunk@15838 bcc190cf-cafb-0310-a4f2-bffc1f526a37

django/conf/locale/__init__.py
@@ -5,6 +5,12 @@ LANG_INFO = {
         'name': 'Arabic',
         'name_local': u'\u0627\u0644\u0639\u0631\u0628\u064a\u0651\u0629',
     },
+    'az': {
+        'bidi': True,
+        'code': 'az',
+        'name': 'Azerbaijani',
+        'name_local': u'az\u0259rbaycan dili',
+    },
     'bg': {
         'bidi': False,
         'code': 'bg',
@@ -82,6 +88,12 @@ LANG_INFO = {
         'code': 'es-ar',
         'name': 'Argentinian Spanish',
         'name_local': u'espa\xf1ol de Argentina',
+    },
+    'es-mx': {
+        'bidi': False,
+        'code': 'es-mx',
+        'name': 'Spanish Mexico',
+        'name_local': u'espa\xf1ol de Mexico',
     },
     'et': {
         'bidi': False,
@@ -358,6 +370,12 @@ LANG_INFO = {
         'code': 'uk',
         'name': 'Ukrainian',
         'name_local': u'\u0423\u043a\u0440\u0430\u0457\u043d\u0441\u044c\u043a\u0430',
+    },
+    'ur': {
+        'bidi': False,
+        'code': 'ur',
+        'name': 'Urdu',
+        'name_local': u'\u0627\u0631\u062f\u0648',
     },
     'vi': {
         'bidi': False,

commit 0893f39d8d59264c53a7eab56129950202f489a5

Fixed mistake introduced in r15838 regarding naming of Mexican Spanish.
git-svn-id: https://code.djangoproject.com/svn/django/trunk@15842 bcc190cf-cafb-0310-a4f2-bffc1f526a37

django/conf/locale/__init__.py
@@ -92,7 +92,7 @@ LANG_INFO = {
     'es-mx': {
         'bidi': False,
         'code': 'es-mx',
-        'name': 'Spanish Mexico',
+        'name': 'Mexican Spanish',
         'name_local': u'espa\xf1ol de Mexico',
     },
     'es-ni': {